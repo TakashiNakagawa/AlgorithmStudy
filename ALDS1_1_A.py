4d28fb6f3cd76b8faa047afa713a6e9ea2806ffc
Takashi Nakagawa <nakagawa@cflat-inc.com>
2016-03-28 05:08:42 +0900

    add alds1_1_a

---
+++ ALDS1_1_A.py
@@ -0,0 +1,28 @@
+
+
+def PrintNumbers(sorted_num):
+    for index, num in enumerate(sorted_num):
+        if index + 1 < len(sorted_num):
+            print(str(num) + " ", end="")
+        else:
+            print(str(num))
+
+
+
+def insertionSort(A, N):
+    sorted_num = list(A)
+    PrintNumbers(sorted_num)
+    for i in range(1, N):
+        j = i - 1
+        v = A[i]
+        while j >= 0 and sorted_num[j] > v:
+            sorted_num[j+1] = sorted_num[j]
+            j -= 1
+        sorted_num[j+1] = A[i]
+        PrintNumbers(sorted_num)
+    return sorted_num
+
+size = int(input())
+nums = [int(x) for x in input().split()]
+
+sorted_num = insertionSort(nums, size)
