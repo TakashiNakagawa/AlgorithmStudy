df8b83d2fb9b8a86136284a85001c19d4bfc332e
Takashi Nakagawa <nakagawa@cflat-inc.com>
2016-03-28 05:16:53 +0900

    模範回答

--- ALDS1_1_A.py
+++ ALDS1_1_A.py
@@ -1,28 +1,41 @@
 
 
-def PrintNumbers(sorted_num):
-    for index, num in enumerate(sorted_num):
-        if index + 1 < len(sorted_num):
-            print(str(num) + " ", end="")
-        else:
-            print(str(num))
+# def PrintNumbers(sorted_num):
+#     for index, num in enumerate(sorted_num):
+#         if index + 1 < len(sorted_num):
+#             print(str(num) + " ", end="")
+#         else:
+#             print(str(num))
+#
+#
+#
+# def insertionSort(A, N):
+#     sorted_num = list(A)
+#     PrintNumbers(sorted_num)
+#     for i in range(1, N):
+#         j = i - 1
+#         v = A[i]
+#         while j >= 0 and sorted_num[j] > v:
+#             sorted_num[j+1] = sorted_num[j]
+#             j -= 1
+#         sorted_num[j+1] = A[i]
+#         PrintNumbers(sorted_num)
+#     return sorted_num
+#
+# size = int(input())
+# nums = [int(x) for x in input().split()]
+#
+# sorted_num = insertionSort(nums, size)
 
 
+n = int(input())
+nums = list(map(int, input().split()))
 
-def insertionSort(A, N):
-    sorted_num = list(A)
-    PrintNumbers(sorted_num)
-    for i in range(1, N):
-        j = i - 1
-        v = A[i]
-        while j >= 0 and sorted_num[j] > v:
-            sorted_num[j+1] = sorted_num[j]
-            j -= 1
-        sorted_num[j+1] = A[i]
-        PrintNumbers(sorted_num)
-    return sorted_num
-
-size = int(input())
-nums = [int(x) for x in input().split()]
-
-sorted_num = insertionSort(nums, size)
+for i in range(len(nums)):
+    key = nums[i]
+    j = i - 1
+    while j >= 0 and nums[j] > key:
+        nums[j+1] = nums[j]
+        j -= 1
+    nums[j+1] = key
+    print(' '.join(map(str, nums)))
